a71ea624e8f1dcd132b1878058817373d04355e7
joserrbv <118081152+joserrbv@users.noreply.github.com>
2023-04-25 23:20:27 -0400

    Criamos o middleware para se conectar ap banco de dados, alem de criar a função para tratar erros esparados

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const conectarBancoDados = require('../middleware/conectarBD');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log("var:", process.env.TEST);
-  res.send('respond with a resource 33' + process.env.TEST );
+router.get('/', conectarBancoDados, function(req, res, next) {
+    res.send('respond with a resource 50' + process.env.TEST );
 });
 
 module.exports = router;
